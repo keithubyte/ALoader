31c6473ca53a68bd606750c91e52aa5931d8aaa6
liangjunfeng <liangjunfeng@ipmacro.com>
2016-06-12 16:55:42 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/loader/LoaderActivity.java
@@ -0,0 +1,13 @@
+package com.demo.loader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loader);
+    }
+}
