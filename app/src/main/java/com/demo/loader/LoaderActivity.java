c9117772132fce6c830122b2348d6a3c742ab84b
liangjunfeng <liangjunfeng@ipmacro.com>
2016-06-12 18:59:51 +0800

    增加打印ClassLoader的代码

--- app/src/main/java/com/demo/loader/LoaderActivity.java
+++ app/src/main/java/com/demo/loader/LoaderActivity.java
@@ -2,6 +2,7 @@ package com.demo.loader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoaderActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class LoaderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loader);
+
+        ClassLoader classLoader = getClassLoader();
+        if (classLoader != null) {
+            do {
+                Log.e("ClassLoader", "classloader = " + classLoader);
+            } while ((classLoader = classLoader.getParent()) != null);
+        }
     }
 }
